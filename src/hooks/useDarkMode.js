5b3fd4a7f25837373534bdb1cb15495380d14a7f
Ashley <54685244+ashleyalmay@users.noreply.github.com>
2020-05-06 19:00:34 -0400

    mvp need to add more notes

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,21 @@
+import { useLocalStorage } from "./useLocalStorage";
+import { useEffect } from "react";
+
+export const useDarkMode = () => {
+    const [values, setValues] = useLocalStorage('dark-mode', false);
+
+    useEffect(() =>{
+        values
+        ? window.document.body.classList.add('dark-mode')
+        :window.document.body.classList.remove('dark-mode');
+    }, [values]);
+// this is how you make the body and class tag with in the use effect
+    const setDarkMode = (event) => {
+        !values ? setValues(true) : setValues(false);
+    };
+ // the event that sets the values on and off or setter function
+
+    return [values, setDarkMode];
+};
+
+// always bring down whasts in the const
